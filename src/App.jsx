7248fdfaf18d85a19d24d4fa0d4f9f1bb4308680
Patrick Menninger <patrick.menninger@gmail.com>
2024-11-25 00:29:37 -0500

    Downloadable

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
+import MainLayout from "./layouts/MainLayout";
 
 const App = () => {
   return (
     <BrowserRouter>
         <Routes>
+            <Route path="/" element={<MainLayout />}>
+            </Route>
             <Route path="/login" element={<LoginPage />}/>
             <Route path="/register" element={<RegisterPage />}/>
         </Routes>
